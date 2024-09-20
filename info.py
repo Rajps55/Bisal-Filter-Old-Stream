026b2d884b263f1eee3c08383af48504aad2067a
Rajps55 <hackinggm69@gmail.com>
2024-09-20 13:30:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,8 +11,8 @@ def is_enabled(value, default):
         return False
     else:
         return default
-PORT = int(environ.get('PORT', 8000))
-NO_PORT = bool(environ.get('NO_PORT', True))
+PORT = int(environ.get('PORT', 5000))
+NO_PORT = bool(environ.get('NO_PORT', False))
 APP_NAME = None
 if 'DYNO' in environ:
     ON_HEROKU = True
